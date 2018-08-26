401d7a7ae5206be197091e6a0f689fa006bd756d
LTLA <aaron.lun@cruk.cam.ac.uk>
2018-08-26 15:08:01 +0100

    Use new resources= argument for BatchtoolsParam.

--- parallel/slurm.R
+++ parallel/slurm.R
@@ -1,3 +1,6 @@
 # Defines a BatchtoolsParam object for submission on a SLURM cluster.
 
-BPPARAM <- BatchtoolsParam(10, cluster="slurm", template="parallel/slurm-aaron.tmpl", logdir="parallel", log=TRUE)
+BPPARAM <- BatchtoolsParam(10, 
+    cluster="slurm", template="parallel/slurm-aaron.tmpl", 
+    logdir="parallel", log=TRUE,
+    resources=list(walltime=20000, memory=8000, ncpus=1))
